7cee205f0b22c2f166b3ec48ca4a1cac8aebe78c
Bhaksar <bhaskarmandal369@gmail.com>
2024-02-10 23:52:33 +0530

    add: cover/not complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/home'
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Hellom World
-      </p>
+      <HomePage/>
     </div>
   );
 }
