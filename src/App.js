23291d17e2e0557dd60f20c22aa4f8b4c95e6f16
Bhaksar <bhaskarmandal369@gmail.com>
2024-02-27 15:30:33 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import HomePage from './pages/home'
-
+import HomePage from './pages/home.tsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFoundPage from './pages/PageNotFound.tsx'
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={<HomePage />}
+          ></Route>
+
+          <Route
+            exact
+            path='/about'
+            element={<HomePage />}
+          ></Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
